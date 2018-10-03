da8395d43cc52a9a0db192f39e35d792c71ca511
23Brenda <brendis_cerebrito@live.com.mx>
2018-10-03 10:42:00 -0500

    rama brenda

---
+++ backend/models/identificacion.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const identificacion = mongoose.Schema({
+  nombre: { type: String, required: true },
+  sexo: { type: String, required: true },
+  edad: { type: String, required: true },
+  estadoCivil: { type: String, required: true },
+  direccion: { type: String, required: true },
+  telefono: { type: String, required: true },
+  ocupacion: { type: String, required: true },
+  lugarDeNacimiento: { type: String, required: true },
+  procedencia: { type: String, required: true },
+  estadoSocioeconomico: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Ident", identificacion);
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule,
+  MatRadioModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { HomeComponent } from './home/home.component';
+import { SidenavFixedComponent } from './Sidenav/sidenav.component';
+import {
+  MatListModule,
+  MatSidenavModule
+ } from '@angular/material';
+import { InicioComponent } from './inicio/inicio.component';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    SidenavFixedComponent,
+    InicioComponent,
+    IdentificacionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatSidenavModule,
+    HttpClientModule,
+    MatRadioModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
