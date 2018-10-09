7878e8f631184121224692848d6fca2d34632171
javieramadorlag <javieramadorlag@hotmail.com>
2018-10-09 11:18:04 -0500

    Componentes1

--- src/app/diagnosticopulpar/diagnosticopulpar.component.ts
+++ src/app/diagnosticopulpar/diagnosticopulpar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { IdentService } from './diagnosticopulpar.service';
 
 @Component({
   selector: 'app-diagnosticopulpar',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diagnosticopulpar.component.css']
 })
 export class DiagnosticopulparComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(public identService: IdentService) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      nombre: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      sexo: new FormControl(null, { validators: [Validators.required] }),
+      edad: new FormControl(null, { validators: [Validators.required] }),
+      estadoCivil: new FormControl(null, { validators: [Validators.required] }),
+      direccion: new FormControl(null, { validators: [Validators.required] }),
+      telefono: new FormControl(null, { validators: [Validators.required] }),
+      ocupacion: new FormControl(null, { validators: [Validators.required] }),
+      lNacimiento: new FormControl(null, { validators: [Validators.required] }),
+      procedencia: new FormControl(null, { validators: [Validators.required] }),
+      estadoSocioeconomico: new FormControl(null, { validators: [Validators.required] }),
+    });
+  }
+
+  addIdent() {
+    this.identService.addIdent(
+      this.form.value.nombre,
+      this.form.value.sexo,
+      this.form.value.edad,
+      this.form.value.estadoCivil,
+      this.form.value.direccion,
+      this.form.value.telefono,
+      this.form.value.ocupacion,
+      this.form.value.lNacimiento,
+      this.form.value.procedencia,
+      this.form.value.estadoSocioeconomico,
+    );
   }
 
 }
