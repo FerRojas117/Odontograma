2c950a41e099427ad6272dfc0013f3c7a7fa2dbd
javieramadorlag <javieramadorlag@hotmail.com>
2018-10-10 11:11:14 -0500

    1111101018

--- src/app/consentimiento/consentimiento.component.ts
+++ src/app/consentimiento/consentimiento.component.ts
@@ -13,6 +13,10 @@ export class ConsentimientoComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       paciente: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      alumno: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      odontologo: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      nombre1: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      nombre2: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
     });
   }
 
