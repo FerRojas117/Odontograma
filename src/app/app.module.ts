6c9829f925724e103c690ec694b4953f84bcfde2
Valeria <valeria@hotmail.com>
2018-10-11 22:45:32 -0500

    add import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import {
   MatExpansionModule,
   MatProgressSpinnerModule,
   MatPaginatorModule,
-  MatRadioModule
+  MatRadioModule,
+  MatSelectModule
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -58,7 +59,8 @@ import { DienteComponent } from './diente/diente.component';
     MatPaginatorModule,
     MatSidenavModule,
     HttpClientModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
