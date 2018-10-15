3a37b654041ea948dd5ae280617821125ccf2559
javieramadorlag <javieramadorlag@hotmail.com>
2018-10-14 22:11:46 -0500

    1008141018

--- src/app/plandetratamiento/plandetratamiento.component.ts
+++ src/app/plandetratamiento/plandetratamiento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-plandetratamiento',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PlandetratamientoComponent implements OnInit {
+  form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      noempleado: new FormControl(null, {validators: [Validators.required] }),
+    });
   }
 
 }
