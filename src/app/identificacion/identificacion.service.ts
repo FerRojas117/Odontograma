c7b8f9ff393ba00097117d118f22ff3d19515290
Itzel Farias <fercho117_25halo@hotmail.com>
2018-10-17 19:58:03 -0500

    CambiosV2

--- src/app/identificacion/identificacion.service.ts
+++ src/app/identificacion/identificacion.service.ts
@@ -11,6 +11,7 @@ export class IdentService {
   private id: string;
 
   private messageUpdated = new Subject<string>();
+  private idUpdated = new Subject<string>();
 
   constructor(
     private http: HttpClient
@@ -45,13 +46,17 @@ export class IdentService {
       .post<{ _id: string, message: string }>('http://localhost:3000/api/ident', registrarIdent)
       .subscribe(responseData => {
         this.id = responseData._id;
+        this.idUpdated.next(this.id);
         this.messageUpdated.next(responseData.message);
       });
   }
 
   getMessageUpdateListener() {
     return this.messageUpdated.asObservable();
- }
+  }
+  getIdUpdateListener() {
+    return this.idUpdated.asObservable();
+  }
 
 }
 
