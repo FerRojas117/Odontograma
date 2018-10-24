34276ee116df5fd95ee77e1e94d8cfd430efe5a4
Valeria <valeria@hotmail.com>
2018-10-24 15:07:48 -0500

    dient

--- src/app/plandetratamiento/plandetratamiento.js
+++ src/app/plandetratamiento/plandetratamiento.js
@@ -1,13 +1,4 @@
-var app = angular.module('app', []);
-
-app.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(|blob|):/);
-}]);
-
-app.controller('appController', function ($scope, $window) {
-    var data = 'Peso:Talla:Edad:',
-        blob = new Blob([data], { type: 'file.xlsx' }),
-        url = $window.URL || $window.webkitURL;
-    $scope.fileUrl = url.createObjectURL(blob);
-    
+angular.module('myApp', [])
+.controller('MainCtrl', function($scope) {
+  $scope.boxClass = true;
 });
