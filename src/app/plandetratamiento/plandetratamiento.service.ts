14a8cc1d10977f94d0970bedd70b1f6f60ec41c1
Itzel Farias <fercho117_25halo@hotmail.com>
2018-10-23 23:41:16 -0500

    COmpletoMerge

--- src/app/plandetratamiento/plandetratamiento.service.ts
+++ src/app/plandetratamiento/plandetratamiento.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-// import { PlandeTratamiento } from '../modelos/plandetratamiento.model';
+import { PlandeTratamiento } from '../modelos/plandetratamiento.model';
 
 
 @Injectable({ providedIn: 'root' })
